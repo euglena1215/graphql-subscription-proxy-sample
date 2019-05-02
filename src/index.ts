09be245625b2c9db8b7c161f6f79d2c3dc200b00
Shintani Teppei <teppest1215@gmail.com>
2019-05-02 11:36:46 +0900

    impl

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,79 @@
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer, gql, PubSub } from "apollo-server";
+import { client as WebSocketClient } from "websocket";
+
+const SEND_CHAT = "SEND_CHAT";
+const WS_SEND_CHAT = "WS_SEND_CHAT";
+
+const pubsub = new PubSub();
+const wsPubsub = new PubSub();
 
 // The GraphQL schema
 const typeDefs = gql`
+  type Chat {
+    handle: String
+    text: String
+  }
   type Query {
-    "A simple type for getting started!"
-    hello: String
+    ping: String
+  }
+
+  type Mutation {
+    sendChat(handle: String, text: String): Chat
+  }
+
+  type Subscription {
+    sentChat: Chat
   }
 `;
 
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    hello: () => "world",
+    ping: () => "pong",
+  },
+  Mutation: {
+    sendChat: (root: any, args: any) => {
+      wsPubsub.publish(WS_SEND_CHAT, args);
+      return args;
+    },
+  },
+  Subscription: {
+    sentChat: {
+      subscribe: () => pubsub.asyncIterator(SEND_CHAT),
+    },
   },
 };
+
+const client = new WebSocketClient();
+
+client.on("connectFailed", function(error) {
+  console.log("Connect Error: " + error.toString());
+});
+
+client.on("connect", function(connection) {
+  console.log("WebSocket Client Connected");
+  connection.on("error", function(error) {
+    console.log("Connection Error: " + error.toString());
+  });
+  connection.on("close", function() {
+    console.log("echo-protocol Connection Closed");
+  });
+  connection.on("message", function(message) {
+    console.log(JSON.parse(message.utf8Data!));
+    if (message.type === "utf8" && message.utf8Data) {
+      console.log("Received: '" + message.utf8Data + "'");
+      pubsub.publish(SEND_CHAT, JSON.parse(message.utf8Data));
+    }
+  });
+
+  wsPubsub.subscribe(WS_SEND_CHAT, (args: { handle: string; text: string }) => {
+    if (connection.connected) {
+      connection.send(JSON.stringify(args));
+    }
+  });
+});
+
+client.connect("ws://localhost:5000/");
 
 const server = new ApolloServer({
   typeDefs,
